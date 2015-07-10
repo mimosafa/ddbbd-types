9b9cd4fba544b92db851907dd65bbadbfbc1bbca
mimoto.t <mimoto@w-tokyodo.com>
2015-07-10 17:56:09 +0900

    Add & Update files. This repository is DDBBD plugins template.

---
+++ lib/singleton.php
@@ -0,0 +1,32 @@
+<?php
+namespace DDBBD;
+
+/**
+ * Singleton trait
+ *
+ * @see http://php.net/manual/ja/language.oop5.traits.php#108293
+ */
+trait Singleton {
+
+	public static function getInstance()
+	{
+		static $instance = null;
+		$class = __CLASS__;
+		return $instance ?: $instance = new $class();
+	}
+
+	protected function __constract()
+	{
+		// Do nothing ( Overwrite, if Necessary )
+	}
+
+	public function __clone()
+	{
+		// Do nothing
+	}
+
+	public function __wakeup() {
+		// Do nothing
+	}
+
+}
